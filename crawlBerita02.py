62ec7976ca98b759a7bd1b077fe704b10622e283
JianFei1 <jianfei2869@gmail.com>
2022-06-17 13:17:17 +0700

    update text-processing pakai yg judul

--- crawlBerita02.py
+++ crawlBerita02.py
@@ -23,6 +23,7 @@ class QuotesSpider(scrapy.Spider):
         # for i in range(1,6):
         yield {
             'judul': response.css('body > div.wrap > div.container.clearfix > div:nth-child(2) > div > h1::text').extract(),
+            'tanggal': response.css('body > div.wrap > div.container.clearfix > div.row.col-offset-fluid.clearfix.js-giant-wp-sticky-parent > div.col-bs10-7.js-read-article > div.read__header.col-offset-fluid.clearfix > div:nth-child(1) > div::text').extract(),
             'label': response.css('body > div.wrap > div.container.clearfix > div:nth-child(2) > div > h3 > ul > li:nth-child(3) > a > span::text').extract(),
             'isi': response.css('body > div.wrap > div.container.clearfix > div.row.col-offset-fluid.clearfix.js-giant-wp-sticky-parent > div.col-bs10-7.js-read-article > div.read__article.mt2.clearfix.js-tower-sticky-parent > div.col-bs9-7 > div.read__content > div > p::text').extract(),
            
